8be2c417e9a8ac05525fc7741c3ae8e70410c142
Florent-Lefevre <lefevre.flo@gmail.com>
2022-03-17 16:46:05 +0100

    add profil route

--- fake_data.sql
+++ fake_data.sql
@@ -3,3 +3,15 @@ INSERT INTO `games` (`name`, `image`, `gifwin`, `gifloose`, `anecdote_win`, `ane
 
 INSERT INTO `games` (`name`, `image`, `gifwin`, `gifloose`, `anecdote_win`, `anecdote_loose`) VALUES
 ('game2', 'image_game2', 'gif_win2', 'gif_loose2', 'anecdote_win_game2', 'anecdote_loose_game2');
+
+INSERT INTO `game_session` (`id_user`, `id_game`, `played`, `score`) VALUES
+('1', '1', true, 5);
+
+INSERT INTO `game_session` (`id_user`, `id_game`, `played`, `score`) VALUES
+('1', '2', true, 6);
+
+INSERT INTO `game_session` (`id_user`, `id_game`, `played`, `score`) VALUES
+('1', '4', true, 4);
+
+INSERT INTO `game_session` (`id_user`, `id_game`, `played`, `score`) VALUES
+('2', '1', true, 4);
